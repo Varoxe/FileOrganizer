65bb78f1d57c7ce3684e7790cf3f204323de5f92
Varoxe <120836510+Varoxe@users.noreply.github.com>
2023-11-23 15:15:19 +0100

    Update FileOrganisation.py
    Updates with pathlib

--- FileOrganisation.py
+++ FileOrganisation.py
@@ -1,49 +1,37 @@
-import os
+from pathlib import Path
 
 # Path to the folder that needs to be organized
-source_directory = r"C:\" # Replace with your own path
+source_directory = Path("C:")  # Replace with your own path
 
 # Mapping from folder name to file extensions
 file_mappings = {
-    'Pictures': ['.jpg', '.png', '.gif'],
+    'Bilder': ['.jpg', '.png', '.gif'],
     'Videos': ['.mp4', '.mov', '.avi'],
-    'Documents': ['.docx', '.pdf', '.txt'],
-    'Programs': ['.exe', '.msi'],
-    'Music': ['.mp3', '.wav'],
+    'Dokumente': ['.docx', '.pdf', '.txt'],
+    'Programme': ['.exe', '.msi'],
+    'Musik': ['.mp3', '.wav'],
     'ZIP': ['.zip', '.rar']
 }
 
 # Create folders if they don't exist
 for folder in file_mappings.keys():
-    folder_path = os.path.join(source_directory, folder)
-    # Check if the folder exists, if not, create it
-    if not os.path.exists(folder_path):
-        os.mkdir(folder_path)
+    folder_path = source_directory / folder
+    folder_path.mkdir(exist_ok=True)  # Create the folder, ignore if it already exists
 
 # Move files to corresponding folders based on their extensions
-for filename in os.listdir(source_directory):
-    # Check if it's a file and not a directory
-    if os.path.isfile(os.path.join(source_directory, filename)):
+for file_path in source_directory.iterdir():
+    if file_path.is_file():
         moved = False  # To track if the file was moved
-        # Go through the mapping to find where to move the file
         for folder, extensions in file_mappings.items():
-            # If the file ends with a known extension, move it
-            if filename.lower().endswith(tuple(extensions)):
-                try:
-                    # Attempt to move the file
-                    os.rename(os.path.join(source_directory, filename),
-                              os.path.join(source_directory, folder, filename))
-                    moved = True  # Indicate success
+            if file_path.suffix.lower() in extensions:
+                target_path = source_directory / folder / file_path.name
+                if not target_path.exists():
+                    file_path.rename(target_path)  # Move the file
+                    moved = True  # Mark as moved
                     break  # No need to check other extensions
-                except FileExistsError as e:
-                    # If file already exists in the target folder, print an error message
-                    print(f"File {filename} already exists in the destination folder {folder}.")
-                except Exception as e:
-                    # For any other exceptions, print an error message
-                    print(f"Error moving {filename}: {e}")
-        # If file didn't fit any category, print a message
+                else:
+                    print(f"File {file_path.name} already exists in the destination folder {folder}.")
         if not moved:
-            print("File not categorized: " + filename)
+            print("File not categorized: " + file_path.name)
 
-# Confirmation message that the process is complete
 print("Files successfully organized!")
